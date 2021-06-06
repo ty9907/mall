ea2f0b4bae37fad3bec3ca4f38813fcdfc4b594c
ty9907 <1729506136@qq.com>
2021-06-06 16:56:39 +0800

    初始化

---
+++ mall-order/src/test/java/com/myself/order/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.order.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
